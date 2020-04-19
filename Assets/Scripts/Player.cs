d0a875fde9fbc030f80a6c1f37798c88957bf768
giribroba <giribroba>
2020-04-19 02:01:32 -0300

    Swipe

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public float velocidade;
+    private float startTouchY, finalTouchY;
+    private Touch touch;
     void Start()
     {
-        
+
     }
     void Update()
     {
-        
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    startTouchY = touch.position.y;
+                    break;
+                case TouchPhase.Moved:
+                    finalTouchY = touch.position.y;
+                    break;
+                case TouchPhase.Ended:
+                    print(startTouchY - finalTouchY < 0 ? "cima" : "baixo");
+                    break;
+            }
+        }
     }
 }
