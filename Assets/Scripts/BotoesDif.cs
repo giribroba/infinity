d13a8e3aeea61b8be298546b1160dcd2bfb5b012
giribroba <giribroba>
2020-04-28 09:25:20 -0300

    Controles corrigidos e menus funcionando

---
+++ Assets/Scripts/BotoesDif.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BotoesDif : MonoBehaviour
+{
+    public static bool som, musica;
+    [SerializeField] private Image iSom, iMusica;
+    [SerializeField] private Sprite[] sSom, sMusica;
+    void Start()
+    {
+        som = (PlayerPrefs.GetInt("som") == 0);
+        musica = (PlayerPrefs.GetInt("musica") == 0);
+        iSom.sprite = sSom[PlayerPrefs.GetInt("som")];
+        iMusica.sprite = sMusica[PlayerPrefs.GetInt("musica")];
+    }
+
+    public void Troca(string tipo)
+    {
+        PlayerPrefs.SetInt(tipo, (PlayerPrefs.GetInt(tipo) == 0 ? 1 : 0));
+        iSom.sprite = sSom[PlayerPrefs.GetInt("som")];
+        iMusica.sprite = sMusica[PlayerPrefs.GetInt("musica")];
+    }
+}
---
+++ Assets/Scripts/BotoesMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class BotoesMenu : MonoBehaviour
+{
+    public static int linguagem;//0 = PT, 1 = ENG
+    void Start()
+    {
+        linguagem = PlayerPrefs.GetInt("lang");
+    }
+
+    public void MudarLang()
+    {
+        PlayerPrefs.SetInt("lang", (linguagem == 1 ? 0 : 1));
+        linguagem = PlayerPrefs.GetInt("lang");
+    }
+
+    public void CarregarCena(string cena)
+    {
+        SceneManager.LoadScene(cena);
+    }
+}
