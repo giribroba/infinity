44da389f7f53310a9bf07ec895976de42f29c4ce
giribroba <giribroba>
2020-05-01 17:00:50 -0300

    telinha sem sprites

---
+++ Assets/Scripts/Telinha.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Telinha : MonoBehaviour
+{
+    [SerializeField] private Sprite[] imagens;
+    private int index;
+    void Update()
+    {
+        if (index >= imagens.Length)
+            this.gameObject.SetActive(false);
+        else
+            this.gameObject.GetComponent<Image>().sprite = imagens[index];
+    }
+
+    public void Avancar()
+    {
+        index++;
+    }
+
+    public void Voltar()
+    {
+        if (index > 0)
+            index--;
+    }
+}
