d9602445503ccef26d44b6e944f45a3e8b66b1c9
giribroba <giribroba>
2020-05-02 02:42:03 -0300

    por hj é só boe

---
+++ Assets/Scripts/MudarGO.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MudarGO : MonoBehaviour
+{
+    [SerializeField] private GameObject[] objetos;
+    void Update()
+    {
+        if (BotoesMenu.linguagem == 0)
+        {
+            objetos[0].SetActive(true);
+            objetos[1].SetActive(false);
+        }
+        else
+        {
+            objetos[1].SetActive(true);
+            objetos[0].SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/Telinha.cs
+++ Assets/Scripts/Telinha.cs
@@ -5,16 +5,23 @@ public class Telinha : MonoBehaviour
 {
     [SerializeField] private Sprite[] imagens;
     [SerializeField] private Image filtro;
+    [SerializeField] private GameObject continuar;
     private int index;
     void Update()
     {
         this.gameObject.GetComponent<Image>().sprite = imagens[index];
+        if(index == imagens.Length - 1)
+            continuar.SetActive(true);
+        else
+            continuar.SetActive(false);
     }
 
     public void Avancar()
     {
         if (index < imagens.Length - 1)
             index++;
+        else
+            Continuar();
     }
 
     public void Voltar()
